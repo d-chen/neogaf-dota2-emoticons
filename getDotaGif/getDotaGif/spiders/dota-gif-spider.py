a52a919f7f026199b013e3b977127c30183eab36
Danny Chen <dannychen210@gmail.com>
2015-04-25 11:53:56 -0700

    remove old parse() from spider

--- getDotaGif/getDotaGif/spiders/dota-gif-spider.py
+++ getDotaGif/getDotaGif/spiders/dota-gif-spider.py
@@ -4,10 +4,6 @@ from getDotaGif.items import GetdotagifItem
 
 raw_url = "https://raw.githubusercontent.com/bontscho/dota2-chat-emoticons/master/assets/images/"
 
-def create_file(myList):
-    with open('./scrape_dota2_gif_list.txt', 'wb') as myFile:
-        json.dump(myList, myFile)
-
 class DotaGifSpider(Spider):
     name = 'dota2gif'
     allowed_domains = ['https://github.com']
@@ -23,16 +19,3 @@ class DotaGifSpider(Spider):
             item['id'] = my_id
             item['src'] = my_src
             yield item
-
-"""
-    def parse(self, response):
-        myList = []
-        for sel in response.xpath("//td[@class='content']//span"):
-            link = sel.xpath('a/@href').extract()[0]
-            file_name = link.split('/')[-1]
-            complete_url = raw_url + file_name
-            if is_32_gif(file_name):
-                myList.append(complete_url)
-
-        create_file(myList)
-"""
