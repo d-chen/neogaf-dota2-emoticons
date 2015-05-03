5fd5b9f02da2c5c1da8db29430fbb64b81f9704b
Danny Chen <dannychen210@gmail.com>
2015-05-03 16:58:23 -0700

    v3.1 : nerfing storm

--- NeoGAF_DOTA2_Emoticons.user.js
+++ NeoGAF_DOTA2_Emoticons.user.js
@@ -7,7 +7,7 @@
 // @include     http://www.neogaf.com/forum/editpost.*
 // @include     http://www.neogaf.com/forum/newreply.*
 // @include     http://www.neogaf.com/forum/newthread.*
-// @version     3.0
+// @version     3.1
 // @grant       GM_log
 // ==/UserScript==
 if (window.top != window.self){ //don't run on frames or iframes
